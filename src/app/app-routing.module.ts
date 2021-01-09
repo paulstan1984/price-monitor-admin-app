721e6de8b9b733d907b0a0caa109b87ac2ec6d5e
Paul Stan <Paul Stan>
2021-01-09 22:15:52 +0200

    Update .gitignore, app-routing.module.ts, and 10 more files...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
