f73cf9774618702a15474166db8ec571a22b8127
Paul Stan <Paul Stan>
2021-02-08 16:33:23 +0200

    Update environment.base.ts and environment.prod.ts

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -2,8 +2,9 @@ export const base_environment = {
     production: false,
     PriceMonitorToken: 'PriceMonitorToken',
 
-    ApiURL: 'http://localhost/price-monitor/api/public/',
-
+    //ApiURL: 'http://localhost/price-monitor/api/public/',
+    ApiURL: 'https://price.priscilla-consulting.ro/public/',    
+    
     DashboardRoute: 'dashboard',
     LoginRoute: 'login',
     ProductsRoute: 'products',
