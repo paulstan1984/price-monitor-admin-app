c57da4cb3789836dfe5eb6add161d3f6b141de5a
Paul Stan <Paul Stan>
2021-07-16 13:22:58 +0300

    Update dashboard.component.html, prices.component.html, and 3 more files...

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -6,7 +6,6 @@ export interface Product extends EditableObject {
     name: string;
     category_id: number;
     category: Category;
-    stores: string;
     lastPrice: number;
     avgPrice: number;
     created_at: Date;
